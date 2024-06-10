7354a4e34f56cc2adb091006c67b29159d64370a
rejoice <you@rejoicepregi2000@gmail.com>
2024-06-10 10:02:49 +0530

    hello

--- question 4.py
+++ question 4.py
@@ -1,6 +1,10 @@
-s = input("number of vowels in string" : )
-count=0
-for i in s:
-    if i=="a"or i=="e" or i=="i" or i=="o" or "u":
+string = input('Enter the vowels :')
+count = 0
+String = string.lower()
+for vowel in String:
+    if vowel == 'a' or vowel == 'e' or vowel == 'i' or vowel == 'o' or vowel == 'u' :
         count+=1
+if count == 0:
+    print('No vowels found')
 else:
+    print('Total numbers of vowels are :' + str(count))
